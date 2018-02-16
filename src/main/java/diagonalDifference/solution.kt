59fa3410d6f7f9aca442df706615f48eff8eb9f0
Kingsley <kingsleykhtam@gmail.com>
2018-02-16 23:01:57 +0800

    initial commit.

---
+++ src/main/java/diagonalDifference/solution.kt
@@ -0,0 +1,7 @@
+package diagonalDifference;
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+}
