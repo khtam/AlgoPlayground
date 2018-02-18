da3ab70a72d9d83ead44829b591a0fff4d83d09f
Kingsley <kingsleykhtam@gmail.com>
2018-02-18 17:29:34 +0800

    format.

---
+++ src/main/java/main.kt
@@ -0,0 +1,23 @@
+
+
+fun main(args: Array<String>) {
+    calculateFractions();
+}
+
+private fun calculateFractions() {
+    val count: Int = readLine()!!.toInt();
+    val input: IntArray = readLine()!!.split(" ").map { it.toInt() }.toIntArray();
+    val (positive, negative, zero) = plusMinus.calculate(count, input);
+    println(positive);
+    println(negative);
+    println(zero);
+}
+
+private fun calculateDiagonalDifference() {
+    val dimension: Int = readLine()!!.toInt();
+    val matrix = Array(dimension) { IntArray(dimension) };
+    for (index in 0 until dimension) {
+        matrix[index] = readLine()!!.split(" ").map(String::toInt).toIntArray()
+    }
+    println("The result is :"+diagonalDifference.calculate(dimension, matrix));
+}
