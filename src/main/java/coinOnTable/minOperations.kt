2972a1e4e841cd02c06d016313f51b8f798148bd
Kingsley <kingsleykhtam@gmail.com>
2018-03-04 14:59:42 +0800

    initial commit.

---
+++ src/main/java/coinOnTable/minOperations.kt
@@ -0,0 +1,5 @@
+package coinOnTable
+
+fun minOperations(row: Int, column: Int, rounds: Int): Int {
+    return 0;
+}
---
+++ src/test/java/coinOnTable/MinOperationsKtTest.kt
@@ -0,0 +1,27 @@
+package coinOnTable
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class MinOperationsKtTest(private val row: Int, private val column: Int, private val rounds: Int, private val expectedResult: Int) {
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data() = listOf(
+                arrayOf(2,2,3,0),
+                arrayOf(2,2,1,1)
+        )
+    }
+
+    @Test
+    fun minOperations() {
+
+        val result = minOperations(row, column, rounds);
+
+        assertThat(result).isEqualTo(expectedResult)
+    }
+}
