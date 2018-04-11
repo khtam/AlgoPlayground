55abbf99b8a8c5e9acfbb559851262ef39e7b557
Kingsley <kingsleykhtam@gmail.com>
2018-04-12 07:56:55 +0800

    coinOnTable: avoid modifying global state.

--- src/main/java/coinOnTable/BoardModifier.kt
+++ src/main/java/coinOnTable/BoardModifier.kt
@@ -16,16 +16,17 @@ class BoardModifier(val destination: Pair<Int, Int>) {
                 .filter { instruction -> !instruction.equals(board[position.first][position.second]) }
                 .filter { instruction -> !instructionPointingOutOfBounds(position, instruction, numberOfRows, numberOfColumns) }
                 .map { instruction ->
-                    board[position.first][position.second] = instruction;
-                    if (isGoalAchieved(board, remainingSteps, destination))
+                    val newBoard = board.copyOf();
+                    newBoard[position.first][position.second] = instruction;
+                    if (isGoalAchieved(newBoard, remainingSteps, destination))
                         return numberOfOperations + 1;
 
                     val movedDown = Pair(position.first + 1, position.second);
-                    val operationsForMovingDownwards = modifyWithLeastNumberOfOperations(board, movedDown, remainingSteps, numberOfOperations + 1);
+                    val operationsForMovingDownwards = modifyWithLeastNumberOfOperations(newBoard, movedDown, remainingSteps, numberOfOperations + 1);
                     listOfOperations.add(operationsForMovingDownwards);
 
                     val movedRight = Pair(position.first, position.second + 1);
-                    val operationsForMovingRight = modifyWithLeastNumberOfOperations(board, movedRight, remainingSteps, numberOfOperations + 1);
+                    val operationsForMovingRight = modifyWithLeastNumberOfOperations(newBoard, movedRight, remainingSteps, numberOfOperations + 1);
                     listOfOperations.add(operationsForMovingRight);
                 }
         val leastAmountOfOperations = listOfOperations
