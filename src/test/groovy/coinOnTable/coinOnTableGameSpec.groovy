86d54949f77fb0a6d662d774d3a0ace5d0a2ea0f
Kingsley <kingsleykhtam@gmail.com>
2018-04-23 07:22:30 +0800

    coinOnTable: updated test coverage.

--- src/test/groovy/coinOnTable/coinOnTableGameSpec.groovy
+++ src/test/groovy/coinOnTable/coinOnTableGameSpec.groovy
@@ -20,4 +20,19 @@ class coinOnTableGameSpec extends Specification {
         "RD*L" | 2   | 2      || [['R', 'D'], ['*', 'L']].toArray() || 1            || 0
     }
 
+    def "should throw exception if input is invalid"() {
+        when:
+        def coinOnTableGame = new CoinOnTableGame(input, row, column);
+
+        then:
+        IllegalArgumentException exception = thrown();
+        exception.message == expectedMessage;
+
+        where:
+        input    | row | column || expectedMessage
+        "R*L"    | 2   | 2      || "Input does not match dimension!"
+        "RRRR*L" | 2   | 2      || "Input does not match dimension!"
+        "R**L"   | 2   | 2      || "Invalid number of destination defined!"
+        "RRUL"   | 2   | 2      || "Invalid number of destination defined!"
+    }
 }
